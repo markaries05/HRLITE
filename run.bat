a4abe562272761c3ddeb88fbd153fb1420b11a2c
markaries05 <53134505+markaries05@users.noreply.github.com>
2025-02-10 22:31:08 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-CD C:\project\HRLITE\Test_cases
+CD Test_cases
 pabot --processes 4 --listener allure_robotframework:test_result/allure-results --outputdir test_result Login.robot
-CD C:\project\HRLITE\Test_cases\test_result
+CD test_result
 allure generate allure-results --clean -o allure-report
